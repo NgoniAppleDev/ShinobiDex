5dabac73365a7e142a2388d2b8ac20d12a85edf1
Ngoni FullStackDev <ngonik8@gmail.com>
2026-07-03 10:07:01 +0200

    Initial Commit

---
+++ ShinobiDex/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShinobiDex
+//
+//  Created by Ngoni Katsidzira  on 3/7/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShinobiDex/ShinobiDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShinobiDexApp.swift
+//  ShinobiDex
+//
+//  Created by Ngoni Katsidzira  on 3/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShinobiDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
